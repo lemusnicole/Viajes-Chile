3016f31a420c964edb3ad8b2a9686af4aad75a35
lemusnicole <nicole.lemusfer@gmail.com>
2022-05-08 20:53:00 -0400

    mi cuarto commit

--- assets/js/javascrip.js
+++ assets/js/javascrip.js
@@ -12,4 +12,5 @@ var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
   return new bootstrap.Popover(popoverTriggerEl)
 })
+//fin JS
 
