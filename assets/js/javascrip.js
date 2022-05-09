6be47b6c68a8ddebc5c760995da00f7ca2da7e61
lemusnicole <nicole.lemusfer@gmail.com>
2022-05-08 20:48:54 -0400

    mi primer commit

---
+++ assets/js/javascrip.js
@@ -0,0 +1,15 @@
+// Cambio de color NavBar
+$(window).scroll(function(){
+    $("nav").toggleClass("scrolled",$(this).scrollTop()>50);
+});
+//Tooltips
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+//Popovers
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
+
